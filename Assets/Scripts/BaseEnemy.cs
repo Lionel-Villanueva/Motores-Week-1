e239e6133eaf4cd02c0cbc8b3eebed72149cdf97
Lionel-Villanueva <salim.villanueva@tecsup.edu.pe>
2026-03-21 16:02:30 -0500

    Avance 2

--- Assets/Scripts/BaseEnemy.cs
+++ Assets/Scripts/BaseEnemy.cs
@@ -2,15 +2,46 @@ using UnityEngine;
 
 public class BaseEnemy : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float Range = 4f;
+    private Transform playerTransform;
+
+
+
     void Start()
     {
-        
+        GetComponent<SphereCollider>().radius = Range;
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            print("Player Detectado");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            print("Player se fue");
+        }
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.purple;
+        Gizmos.DrawWireSphere(transform.position, Range);
+
+        if (playerTransform != null)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawLine(transform.position, playerTransform.position);
+
+        }
+
     }
 }
--- Assets/Scripts/PROBANDO.cs
+++ Assets/Scripts/PROBANDO.cs
@@ -2,17 +2,7 @@ using UnityEngine;
 
 public class PROBANDO : MonoBehaviour
 {
-
-    [SerializeField] private GameObject prefab;
-
-
-
-
-
-
-
-
-
+    public LayerMask enemyLayer;
 
     void Start()
     {
@@ -21,6 +11,19 @@ public class PROBANDO : MonoBehaviour
 
     void Update()
     {
+        Ray ray = new Ray(transform.position, transform.forward);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, 100f, enemyLayer))
+        {
+            Debug.Log("Enemy Detected");
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, 5f);
+        Gizmos.color = Color.blue;
         
     }
 }
